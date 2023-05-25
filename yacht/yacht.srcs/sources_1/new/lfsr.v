2aabfe966b63d6926f685292c0053563f3a712fd
sohnryang <loop.infinitely@gmail.com>
2023-05-25 22:31:31 +0900

    feat(lfsr): add 16-bit LFSR implementation

--- yacht/yacht.srcs/sources_1/new/lfsr.v
+++ yacht/yacht.srcs/sources_1/new/lfsr.v
@@ -19,3 +19,23 @@ module lfsr_4b(
     end
     assign out = outreg;
 endmodule
+
+// 16-bit LFSR module.
+// Assumes positive-edge clock and negative-edge reset.
+module lfsr_16b(
+    input clk,
+    input rst_n,
+    output [15:0] out
+    );
+    reg [15:0] outreg;
+    wire feedback;
+    assign feedback = ~(out[3] ^ out[12] ^ out[14] ^ out[15]);
+    
+    always @(posedge clk, negedge rst_n) begin
+        if (!rst_n)
+            outreg = 16'b0;
+        else
+            outreg = {outreg[14:0], feedback};
+    end
+    assign out = outreg;
+endmodule
