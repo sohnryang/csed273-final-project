fb9795349edfeaea89764fee98f890fce86af13f
sohnryang <loop.infinitely@gmail.com>
2023-05-30 00:50:58 +0900

    fix(fsm): fix errors in sources

---
+++ yacht/yacht.srcs/sim_1/new/fsm_testbench.v
@@ -0,0 +1,30 @@
+// fsm_ testbench
+
+module statefsm_tb();
+    reg clk, reset_n;
+    always begin
+        #5
+        clk = ~clk;
+    end
+
+    reg [1:0] fsm_input;
+    wire [3:0] state;
+    
+    initial begin
+        clk = 0;
+        reset_n = 0;
+        #15 reset_n = 1;
+        fsm_input = 2'b00;
+        #10 fsm_input = 2'b10;
+        #10 fsm_input = 2'b01;
+        #10 fsm_input = 2'b11;
+        #10
+        $finish;
+    end
+    
+    always @(negedge clk) begin
+        $display("Current state: %b", state);
+    end
+    
+    statefsm fsm(clk, reset_n, fsm_input, state);
+endmodule
--- yacht/yacht.srcs/sources_1/new/fsm.v
+++ yacht/yacht.srcs/sources_1/new/fsm.v
@@ -4,12 +4,11 @@
 
 module statefsm(
     input wire clk,
-    input wire reset, 
-    input wire [1:0] input, 
+    input wire reset_n,
+    input wire [1:0] fsm_input,
     output reg [3:0] state
 );
-
-// state 정의
+    // State definition
     parameter S0 = 4'b0000; 
     parameter S1 = 4'b0001;
     parameter S2 = 4'b0010;
@@ -24,140 +23,121 @@ module statefsm(
     parameter S11 = 4'b1011;
 
 
-    always @ (negedge clk or negedge reset)
-    begin
-        if (reset)
-            state <= S0; // reset이면 S0 state으로 초기화
-    end
+    always @ (negedge clk or negedge reset_n) begin
+        if (!reset_n) begin
+            state <= S0; // init to S0 on reset
+        end
         else begin
             case (state)
             S0: begin
-                if(intput == 2'b00)// 그대로
-                state <= S0;
-                else if(input == 2'b10) // 다음으로
-                state <= S1;
-                else if(intput == 2'b01) // 이전으로
-                state <= S11;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S0;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S1;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S11;
                 // else dont care
             end
             S1: begin
-                if(intput == 2'b00)// 그대로
-                state <= S1;
-                else if(input == 2'b10) // 다음으로
-                state <= S2;
-                else if(intput == 2'b01) // 이전으로
-                state <= S0;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S1;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S2;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S0;
                 // else dont care
             end
             S2: begin
-                if(intput == 2'b00)// 그대로
-                state <= S2;
-                else if(input == 2'b10) // 다음으로
-                state <= S3;
-                else if(intput == 2'b01) // 이전으로
-                state <= S1;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S2;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S3;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S1;
                 // else dont care
             end
             S3: begin
-                if(intput == 2'b00)// 그대로
-                state <= S3;
-                else if(input == 2'b10) // 다음으로
-                state <= S4;
-                else if(intput == 2'b01) // 이전으로
-                state <= S2;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S3;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S4;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S2;
                 // else dont care
             end
             S4: begin
-                if(intput == 2'b00)// 그대로
-                state <= S4;
-                else if(input == 2'b10) // 다음으로
-                state <= S5;
-                else if(intput == 2'b01) // 이전으로
-                state <= S3;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S4;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S5;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S3;
                 // else dont care
             end
             S5: begin
-                if(intput == 2'b00)// 그대로
-                state <= S5;
-                else if(input == 2'b10) // 다음으로
-                state <= S6;
-                else if(intput == 2'b01) // 이전으로
-                state <= S4;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S5;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S6;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S4;
                 // else dont care
             end
             S6: begin
-                if(intput == 2'b00)// 그대로
-                state <= S6;
-                else if(input == 2'b10) // 다음으로
-                state <= S7;
-                else if(intput == 2'b01) // 이전으로
-                state <= S5;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S6;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S7;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S5;
                 // else dont care
             end
             S7: begin
-                if(intput == 2'b00)// 그대로
-                state <= S7;
-                else if(input == 2'b10) // 다음으로
-                state <= S8;
-                else if(intput == 2'b01) // 이전으로
-                state <= S6;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S7;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S8;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S6;
                 // else dont care
             end
             S8: begin
-                if(intput == 2'b00)// 그대로
-                state <= S8;
-                else if(input == 2'b10) // 다음으로
-                state <= S9;
-                else if(intput == 2'b01) // 이전으로
-                state <= S7;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S8;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S9;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S7;
                 // else dont care
             end
             S9: begin
-                if(intput == 2'b00)// 그대로
-                state <= S9;
-                else if(input == 2'b10) // 다음으로
-                state <= S10;
-                else if(intput == 2'b01) // 이전으로
-                state <= S8;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S9;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S10;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S8;
                 // else dont care
             end
             S10: begin
-                if(intput == 2'b00)// 그대로
-                state <= S10;
-                else if(input == 2'b10) // 다음으로
-                state <= S11;
-                else if(intput == 2'b01) // 이전으로
-                state <= S9;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S10;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S11;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S9;
                 // else dont care
             end
             S11: begin
-                if(intput == 2'b00)// 그대로
-                state <= S11;
-                else if(input == 2'b10) // 다음으로
-                state <= S0;
-                else if(intput == 2'b01) // 이전으로
-                state <= S10;
+                if(fsm_input == 2'b00) // do nothing
+                    state <= S11;
+                else if(fsm_input == 2'b10) // next state
+                    state <= S0;
+                else if(fsm_input == 2'b01) // prev state
+                    state <= S10;
                 // else dont care
             end
             endcase
         end
-
-// output 출력 로직
-always @ (state) begin
-    case(state)
-        S0: output = 4'b0000;
-        S1: output = 4'b0001;
-        S2: output = 4'b0010;
-        S3: output = 4'b0011;
-        S4: output = 4'b0100;
-        S5: output = 4'b0101;
-        S6: output = 4'b0110;
-        S7: output = 4'b0111;
-        S8: output = 4'b1000;
-        S9: output = 4'b1001;
-        S10: output = 4'b1010;
-        S11: output = 4'b1011;
-    endcase
-end
-
-end module
+    end
+endmodule
