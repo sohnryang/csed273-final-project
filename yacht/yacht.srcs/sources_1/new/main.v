f0659578b9a334d703d530372ad5420074a4a2ee
sohnryang <loop.infinitely@gmail.com>
2023-05-30 00:15:42 +0900

    feat(main): add entry point stub

---
+++ yacht/yacht.srcs/sources_1/new/main.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module yacht_main(
+    input clk,
+    input next_button,
+    input prev_button,
+    input throw_button,
+    output [7:0] ss_display,
+    output [3:0] ss_sel
+    );
+    reg reset_n;
+    reg [1:0] initial_clock_count;
+    reg [14:0] current_dice_bits;
+    wire [14:0] dice_rng_output;
+    
+    // Initialize registers.
+    initial begin
+        reset_n <= 0;
+        initial_clock_count <= 2'b0;
+        current_dice_bits <= 15'b0;
+    end
+    
+    // Reset on startup.
+    always @(posedge clk) begin
+        if (initial_clock_count < 2'b11) begin
+            initial_clock_count = initial_clock_count + 1;
+        end
+        else if(initial_clock_count == 2'b11) begin
+            reset_n <= 1;
+        end
+    end
+    
+    // Update currently saved dice results while button is pressed.
+    always @(posedge clk) begin
+        if (throw_button)
+            current_dice_bits <= dice_rng_output;
+    end
+    
+    yacht_dices dice_rng(
+        clk,
+        reset_n,
+        dice_rng_output[ 2: 0],
+        dice_rng_output[ 5: 3],
+        dice_rng_output[ 8: 6],
+        dice_rng_output[11: 9],
+        dice_rng_output[14:12]
+    );
+endmodule
