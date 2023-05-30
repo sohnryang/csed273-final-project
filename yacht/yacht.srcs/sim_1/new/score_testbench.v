2f1fbe83b4678867e5e5bd580ee71d043e8c48cf
sohnryang <loop.infinitely@gmail.com>
2023-05-30 16:04:59 +0900

    fix(score): fix incorrect wire type

--- yacht/yacht.srcs/sim_1/new/score_testbench.v
+++ yacht/yacht.srcs/sim_1/new/score_testbench.v
@@ -2,8 +2,7 @@
 
 module score_testbench();
     reg [5:0] dice_0, dice_1, dice_2, dice_3, dice_4;
-    wire [5:0] little_straight_out;
-    wire big_straight_out, yacht_out;
+    wire [5:0] little_straight_out, big_straight_out, yacht_out;
     initial begin
         // 4, 3, 5, 2, 1
         dice_0 <= 6'b001000;
