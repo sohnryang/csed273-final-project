6caf306d0074d94a1b3dd2a5b7912b56b2495a0f
sohnryang <loop.infinitely@gmail.com>
2023-05-30 15:55:58 +0900

    feat(main): add scoring modules and mux to main

--- yacht/yacht.srcs/sources_1/new/main.v
+++ yacht/yacht.srcs/sources_1/new/main.v
@@ -15,6 +15,10 @@ module yacht_main(
     wire [14:0] dice_rng_output;
     wire [3:0] score_type_state;
     wire [1:0] fsm_input;
+    wire [5:0] current_dice_onehot_0, current_dice_onehot_1, current_dice_onehot_2,
+               current_dice_onehot_3, current_dice_onehot_4;
+    wire [5:0] litte_straight_score, big_straight_score, yacht_score;
+    reg [5:0] selected_score;
     
     // Initialize registers.
     initial begin
@@ -57,4 +61,49 @@ module yacht_main(
     assign fsm_input[0] = prev_button;
     assign fsm_input[1] = next_button;
     statefsm score_type_fsm(clk, reset_n, fsm_input, score_type_state);
+    dice_decode dec0(current_dice_bits[ 2: 0], current_dice_onehot_0);
+    dice_decode dec1(current_dice_bits[ 5: 3], current_dice_onehot_1);
+    dice_decode dec2(current_dice_bits[ 8: 6], current_dice_onehot_2);
+    dice_decode dec3(current_dice_bits[11: 9], current_dice_onehot_3);
+    dice_decode dec4(current_dice_bits[14:12], current_dice_onehot_4);
+    score_little_straight score8(
+        current_dice_onehot_0,
+        current_dice_onehot_1,
+        current_dice_onehot_2,
+        current_dice_onehot_3,
+        current_dice_onehot_4,
+        little_straight_score
+    );
+    score_big_straight score9(
+        current_dice_onehot_0,
+        current_dice_onehot_1,
+        current_dice_onehot_2,
+        current_dice_onehot_3,
+        current_dice_onehot_4,
+        big_straight_score
+    );
+    score_yacht score10(
+        current_dice_onehot_0,
+        current_dice_onehot_1,
+        current_dice_onehot_2,
+        current_dice_onehot_3,
+        current_dice_onehot_4,
+        yacht_score
+    );
+    always @(score_type_state) begin
+        case (score_type_state)
+            4'd0: selected_score <= 6'd42;
+            4'd1: selected_score <= 6'd42;
+            4'd2: selected_score <= 6'd42;
+            4'd3: selected_score <= 6'd42;
+            4'd4: selected_score <= 6'd42;
+            4'd5: selected_score <= 6'd42;
+            4'd6: selected_score <= 6'd42;
+            4'd7: selected_score <= 6'd42;
+            4'd8: selected_score <= little_straight_score;
+            4'd9: selected_score <= big_straight_score;
+            4'd10: selected_score <= yacht_score;
+            4'd11: selected_score <= 6'd42;
+        endcase
+    end
 endmodule
