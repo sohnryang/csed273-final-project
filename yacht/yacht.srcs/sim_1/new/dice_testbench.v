876f2b826e1311bcb289f6a2c01d6583a46d4041
sohnryang <loop.infinitely@gmail.com>
2023-05-30 15:42:10 +0900

    test(dice): add decoder for testing

--- yacht/yacht.srcs/sim_1/new/dice_testbench.v
+++ yacht/yacht.srcs/sim_1/new/dice_testbench.v
@@ -4,6 +4,7 @@
 module dice_testbench();
     reg clk, rst_n;
     wire [2:0] dice0, dice1, dice2, dice3, dice4;
+    wire [29:0] one_hot;
     initial begin
         clk = 0;
         rst_n = 0;
@@ -19,4 +20,9 @@ module dice_testbench();
     end
     
     yacht_dices d(clk, rst_n, dice0, dice1, dice2, dice3, dice4);
+    dice_decode dec0(dice0, one_hot[ 5: 0]);
+    dice_decode dec1(dice1, one_hot[11: 6]);
+    dice_decode dec2(dice2, one_hot[17:12]);
+    dice_decode dec3(dice3, one_hot[23:18]);
+    dice_decode dec4(dice4, one_hot[29:24]);
 endmodule
