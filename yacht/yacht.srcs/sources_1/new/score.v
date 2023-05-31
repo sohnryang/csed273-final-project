8c7eac5fa09679d683ab372c1551d85f8ee5baa5
sohnryang <loop.infinitely@gmail.com>
2023-05-31 21:29:05 +0900

    fix(score): fix adder and add more types of scores

--- yacht/yacht.srcs/sources_1/new/score.v
+++ yacht/yacht.srcs/sources_1/new/score.v
@@ -1,81 +1,111 @@
 `timescale 1ns / 1ps
 
-module fulladder(input in_a,input in_b,input in_c, output out_s, output out_c);
-    assign out_s = in_a ^ in_b ^ in_c;
-    assign out_c = in_a & in_b | in_b & in_c | in_c & in_a;
-endmodule
+module adder(
+    input [5:0] x,
+    input [5:0] y,
+    input c_in,             // Carry in
+    output [5:0] out,
+    output c_out            // Carry out
+);
+    wire [5:0] sum, half_carry1, half_carry2; // wires to hold intermediate results
+    wire [6:0] intermediate_carrys; // intermediate carry outputs of full adders
+    assign intermediate_carrys[0] = c_in; // first carry is carry input
+    assign sum = x ^ y; // carryless sums are just xor
+    assign half_carry1 = x & y; // carry outputs of first half adders
 
-module adder(input[2:0] add_1, input add_2, output [2:0]sum);
-    wire c_1,c_2,c_3;
-    fulladder FA1(add_1[0],add_2,1'b0,sum[0],c_1);
-    fulladder FA2(add_1[1],1'b0,c_1,sum[1],c_2);
-    fulladder FA3(add_1[2],1'b0,c_2,sum[2],c_3);
-endmodule
+    // second half adder run for bit 0
+    assign out[0] = sum[0] ^ intermediate_carrys[0];
+    assign half_carry2[0] = sum[0] & intermediate_carrys[0];
+    assign intermediate_carrys[1] = half_carry1[0] | half_carry2[0];
 
-module sixbitadder(
-    input [5:0] in_a,
-    input [5:0] in_b,
-    input in_c,
-    output [5:0] out_s,
-    output out_c
-    );
-    wire [5:0] c;
-    assign c[0] = in_c;
+    // second half adder run for bit 1
+    assign out[1] = sum[1] ^ intermediate_carrys[1];
+    assign half_carry2[1] = sum[1] & intermediate_carrys[1];
+    assign intermediate_carrys[2] = half_carry1[1] | half_carry2[1];
 
-    fullAdder f1(in_a[0], in_b[0], c[0], out_s[0], c[1]);
-    fullAdder f2(in_a[1], in_b[1], c[1], out_s[1], c[2]);
-    fullAdder f3(in_a[2], in_b[2], c[2], out_s[2], c[3]);
-    fullAdder f4(in_a[3], in_b[3], c[3], out_s[3], c[4]);
-    fullAdder f5(in_a[4], in_b[4], c[4], out_s[4], c[5]);
-    fullAdder f6(in_a[5], in_b[5], c[5], out_s[5], out_c);
+    // second half adder run for bit 2
+    assign out[2] = sum[2] ^ intermediate_carrys[2];
+    assign half_carry2[2] = sum[2] & intermediate_carrys[2];
+    assign intermediate_carrys[3] = half_carry1[2] | half_carry2[2];
+
+    // second half adder run for bit 3
+    assign out[3] = sum[3] ^ intermediate_carrys[3];
+    assign half_carry2[3] = sum[3] & intermediate_carrys[3];
+    assign intermediate_carrys[4] = half_carry1[3] | half_carry2[3];
+
+    // second half adder run for bit 4
+    assign out[4] = sum[4] ^ intermediate_carrys[4];
+    assign half_carry2[4] = sum[4] & intermediate_carrys[4];
+    assign intermediate_carrys[5] = half_carry1[4] | half_carry2[4];
+    
+    // second half adder run for bit 5
+    assign out[5] = sum[5] ^ intermediate_carrys[5];
+    assign half_carry2[5] = sum[5] & intermediate_carrys[5];
+    assign intermediate_carrys[6] = half_carry1[5] | half_carry2[5];
+    
+    // carry out
+    assign c_out = intermediate_carrys[6];
 endmodule
 
 module score_ones(
+    input [5:0] dice_0,
     input [5:0] dice_1,
     input [5:0] dice_2,
     input [5:0] dice_3,
     input [5:0] dice_4,
-    input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    adder CA1(3'b000,dice_1[0],point);
-    adder CA2(point,dice_2[0],point);
-    adder CA3(point,dice_3[0],point);
-    adder CA4(point,dice_4[0],point);
-    adder CA5(point,dice_0[0],point_out);
+    wire [5:0] sum_0, sum_1, sum_2, sum_3;
+    wire [4:0] couts;
+    adder CA1(6'b0, 6'b0, dice_0[0], sum_0, couts[0]);
+    adder CA2(sum_0, 6'b0, dice_1[0], sum_1, couts[1]);
+    adder CA3(sum_1, 6'b0, dice_2[0], sum_2, couts[2]);
+    adder CA4(sum_2, 6'b0, dice_3[0], sum_3, couts[3]);
+    adder CA5(sum_3, 6'b0, dice_4[0], point_out, couts[4]);
 endmodule
 
 module score_twos(
+    input [5:0] dice_0,
     input [5:0] dice_1,
     input [5:0] dice_2,
     input [5:0] dice_3,
     input [5:0] dice_4,
-    input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    counter CT1(3'b000,dice_1[1],point);
-    counter CT2(point,dice_2[1],point);
-    counter CT3(point,dice_3[1],point);
-    counter CT4(point,dice_4[1],point);
-    counter CT5(point,dice_0[1],point_out);
+    wire [5:0] op0, op1, op2, op3, op4;
+    assign op0 = dice_0[1] ? 6'd2 : 6'b0;
+    assign op1 = dice_1[1] ? 6'd2 : 6'b0;
+    assign op2 = dice_2[1] ? 6'd2 : 6'b0;
+    assign op3 = dice_3[1] ? 6'd2 : 6'b0;
+    assign op4 = dice_4[1] ? 6'd2 : 6'b0;
+    wire [5:0] sum_0, sum_1, sum_2;
+    wire [3:0] couts;
+    adder CA1(op0, op1, 6'b0, sum_0, couts[0]);
+    adder CA2(sum_0, op2, 6'b0, sum_1, couts[1]);
+    adder CA3(sum_1, op3, 6'b0, sum_2, couts[2]);
+    adder CA4(sum_2, op4, 6'b0, point_out, couts[3]);
 endmodule
 
 module score_threes(
+    input [5:0] dice_0,
     input [5:0] dice_1,
     input [5:0] dice_2,
     input [5:0] dice_3,
     input [5:0] dice_4,
-    input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    counter CTh1(3'b000,dice_1[1],point);
-    counter CTh2(point,dice_2[1],point);
-    counter CTh3(point,dice_3[1],point);
-    counter CTh4(point,dice_4[1],point);
-    counter CTh5(point,dice_0[1],point_out);
+    wire [5:0] op0, op1, op2, op3, op4;
+    assign op0 = dice_0[2] ? 6'd3 : 6'b0;
+    assign op1 = dice_1[2] ? 6'd3 : 6'b0;
+    assign op2 = dice_2[2] ? 6'd3 : 6'b0;
+    assign op3 = dice_3[2] ? 6'd3 : 6'b0;
+    assign op4 = dice_4[2] ? 6'd3 : 6'b0;
+    wire [5:0] sum_0, sum_1, sum_2;
+    wire [3:0] couts;
+    adder CA1(op0, op1, 6'b0, sum_0, couts[0]);
+    adder CA2(sum_0, op2, 6'b0, sum_1, couts[1]);
+    adder CA3(sum_1, op3, 6'b0, sum_2, couts[2]);
+    adder CA4(sum_2, op4, 6'b0, point_out, couts[3]);
 endmodule
 
 module score_fours(
@@ -84,14 +114,20 @@ module score_fours(
     input [5:0] dice_3,
     input [5:0] dice_4,
     input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    counter CF1(3'b000,dice_1[1],point);
-    counter CF2(point,dice_2[1],point);
-    counter CF3(point,dice_3[1],point);
-    counter CF4(point,dice_4[1],point);
-    counter CF5(point,dice_0[1],point_out);
+    wire [5:0] op0, op1, op2, op3, op4;
+    assign op0 = dice_0[3] ? 6'd4 : 6'b0;
+    assign op1 = dice_1[3] ? 6'd4 : 6'b0;
+    assign op2 = dice_2[3] ? 6'd4 : 6'b0;
+    assign op3 = dice_3[3] ? 6'd4 : 6'b0;
+    assign op4 = dice_4[3] ? 6'd4 : 6'b0;
+    wire [5:0] sum_0, sum_1, sum_2;
+    wire [3:0] couts;
+    adder CA1(op0, op1, 6'b0, sum_0, couts[0]);
+    adder CA2(sum_0, op2, 6'b0, sum_1, couts[1]);
+    adder CA3(sum_1, op3, 6'b0, sum_2, couts[2]);
+    adder CA4(sum_2, op4, 6'b0, point_out, couts[3]);
 endmodule
 
 module score_fives(
@@ -100,14 +136,20 @@ module score_fives(
     input [5:0] dice_3,
     input [5:0] dice_4,
     input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    counter CFi1(3'b000,dice_1[1],point);
-    counter CFi2(point,dice_2[1],point);
-    counter CFi3(point,dice_3[1],point);
-    counter CFi4(point,dice_4[1],point);
-    counter CFi5(point,dice_0[1],point_out);
+    wire [5:0] op0, op1, op2, op3, op4;
+    assign op0 = dice_0[4] ? 6'd5 : 6'b0;
+    assign op1 = dice_1[4] ? 6'd5 : 6'b0;
+    assign op2 = dice_2[4] ? 6'd5 : 6'b0;
+    assign op3 = dice_3[4] ? 6'd5 : 6'b0;
+    assign op4 = dice_4[4] ? 6'd5 : 6'b0;
+    wire [5:0] sum_0, sum_1, sum_2;
+    wire [3:0] couts;
+    adder CA1(op0, op1, 6'b0, sum_0, couts[0]);
+    adder CA2(sum_0, op2, 6'b0, sum_1, couts[1]);
+    adder CA3(sum_1, op3, 6'b0, sum_2, couts[2]);
+    adder CA4(sum_2, op4, 6'b0, point_out, couts[3]);
 endmodule
 
 module score_sixes(
@@ -116,14 +158,20 @@ module score_sixes(
     input [5:0] dice_3,
     input [5:0] dice_4,
     input [5:0] dice_0,
-    output [2:0] point_out
+    output [5:0] point_out
     );
-    wire [2:0] point;
-    counter CS1(3'b000,dice_1[1],point);
-    counter CS2(point,dice_2[1],point);
-    counter CS3(point,dice_3[1],point);
-    counter CS4(point,dice_4[1],point);
-    counter CS5(point,dice_0[1],point_out);
+    wire [5:0] op0, op1, op2, op3, op4;
+    assign op0 = dice_0[5] ? 6'd6 : 6'b0;
+    assign op1 = dice_1[5] ? 6'd6 : 6'b0;
+    assign op2 = dice_2[5] ? 6'd6 : 6'b0;
+    assign op3 = dice_3[5] ? 6'd6 : 6'b0;
+    assign op4 = dice_4[5] ? 6'd6 : 6'b0;
+    wire [5:0] sum_0, sum_1, sum_2;
+    wire [3:0] couts;
+    adder CA1(op0, op1, 6'b0, sum_0, couts[0]);
+    adder CA2(sum_0, op2, 6'b0, sum_1, couts[1]);
+    adder CA3(sum_1, op3, 6'b0, sum_2, couts[2]);
+    adder CA4(sum_2, op4, 6'b0, point_out, couts[3]);
 endmodule
 
 module score_four_of_a_kind(
@@ -240,12 +288,11 @@ module score_choice(
     input [2:0] dice_4,
     output [5:0] point_out
     );
-    wire [5:0] sum_0;
-    wire [5:0] sum_1;
-    wire [5:0] sum_2;
-    wire [3:0]c;
-    sixbitadder SA1({0,0,0,dice_0},{0,0,0,dice_1},1'b0,sum_0,c[0]);
-    sixbitadder SA2(sum_0,{0,0,0,dice_2},c[0],sum_1,c[1]);
-    sixbitadder SA3(sum_1,{0,0,0,dice_3},c[1],sum_2,c[2]);
-    sixbitadder SA4(sum_2,{0,0,0,dice_4},c[2],point_sum,c[3]);
+    wire [5:0] sum_0, sum_1, sum_2, sum_3;
+    wire [4:0] c;
+    adder SA1({0, 0, 0, dice_0}, {0, 0, 0, dice_1}, 0, sum_0, c[0]);
+    adder SA2(sum_0, {0, 0, 0, dice_2}, 0, sum_1, c[1]);
+    adder SA3(sum_1, {0, 0, 0, dice_3}, 0, sum_2, c[2]);
+    adder SA4(sum_2, {0, 0, 0, dice_4}, 0, sum_3, c[3]);
+    adder SA5(sum_3, 6'd5, 0, point_out, c[4]);
 endmodule
